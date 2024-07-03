fceed680bdf91ebcef577af94f79b34347df7666
Maciej <maclis124@gmail.com>
2024-07-03 07:15:04 +0200

    Initial commit.

---
+++ src/db/__init__.py
@@ -0,0 +1,6 @@
+"""DB-related utils for the app."""
+from .models import Base
+
+__all__ = [
+    "Base"
+]
---
+++ src/db/models.py
@@ -0,0 +1,4 @@
+"""ORM models for the app."""
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
