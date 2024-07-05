9c36a9053ac3edace251852e3fd52297d4371a18
Maciej <maclis124@gmail.com>
2024-07-05 14:27:39 +0200

    Define models for recipes and create migrations.

--- src/db/models.py
+++ src/db/models.py
@@ -2,8 +2,9 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column, Date, Integer, String, Text
+from sqlalchemy import Boolean, Column, Date, Float, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 from src.roles import Roles
 
@@ -23,3 +24,70 @@ class DB_User(Base):
     create_date = Column(Date, default=datetime.now())
     date_of_birth = Column(Date)
     role = Column(Integer, default=Roles.USER.value)
+
+    recipes = relationship("Recipe", back_populates="author")
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    recipe_id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    create_date = Column(Date, default=datetime.now(), nullable=False)
+    servings = Column(Integer, nullable=False)
+    prep_time = Column(Integer, nullable=False)
+    description = Column(Text, nullable=False)
+
+    author = relationship("User", back_populates="recipes")
+    nutrition_info = relationship("NutritionInfo", back_populates="recipe")
+    instructions = relationship("Instruction", back_populates="recipe")
+    ingredientes = relationship("Ingredient", back_populates="recipe")
+
+
+class NutritionInfo(Base):
+    __tablename__ = "nutrition_infos"
+
+    nutritio_info_id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey("recipes.recipe_id"), nullable=False)
+    calories = Column(Integer, nullable=False)
+    protein = Column(Integer, nullable=False)
+    carbohydrates = Column(Integer, nullable=False)
+    sugar = Column(Integer, nullable=False)
+    fiber = Column(Integer, nullable=False)
+    fat = Column(Integer, nullable=False)
+
+    recipe = relationship("Recipes", back_populates="nutrition_info")
+
+
+class Instruction(Base):
+    __tablename__ = "instructions"
+
+    instruction_id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey("recipes.recipe_id"), nullable=False)
+    text = Column(Text, nullable=False)
+    order = Column(Integer, nullable=False)
+
+    recipe = relationship("Recipe", back_populates="instructions")
+
+
+class Unit(Base):
+    __tablename__ = "units"
+
+    unit_id = Column(Integer, primary_key=True)
+    unit = Column(String(100), nullable=False)
+    liquid = Column(Boolean, nullable=False)
+
+    ingredientes = relationship("Ingredient", back_populates="unit")
+
+
+class Ingredient(Base):
+    __tablename__ = "ingredientes"
+
+    ingredient_id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey("recipes.recipe_id"), nullable=False)
+    ingredient = Column(String(100), nullable=False)
+    amount = Column(Float(2), nullable=False)
+    unit_id = Column(Integer, ForeignKey("units.unit_id"))
+
+    recipe = relationship("Recipe", back_populates="ingredientes")
+    unit = relationship("Unit", back_populates="ingredientes")
