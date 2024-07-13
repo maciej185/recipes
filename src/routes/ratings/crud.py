07f1a62638ad77fcc644a6516a47c62e3deb66ef
Maciej <maclis124@gmail.com>
2024-07-13 11:34:03 +0200

    Create an endpoint for deleting ratings.

--- src/routes/ratings/crud.py
+++ src/routes/ratings/crud.py
@@ -1,10 +1,13 @@
 """CRUD operations for the ratingd package."""
 
-from fastapi import HTTPException, status
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Path, status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from src.db.models import DB_Rating
+from src.dependencies import get_db
 
 from .models import RatingAdd
 
@@ -22,3 +25,28 @@ def create_rating_in_db(db: Session, rating_data: RatingAdd, author_id: int) -> 
         )
     db.refresh(db_rating)
     return db_rating
+
+
+def get_rating_from_db(
+    rating_id: Annotated[int, Path()], db: Annotated[Session, Depends(get_db)]
+) -> DB_Rating:
+    """Get rating with the given ID.
+
+    Used as a dependedncy in path operations.
+
+    Raises:
+        HTTPException: Raised when a rating with the given ID was not found in the DB.
+    """
+    db_rating = db.query(DB_Rating).filter(DB_Rating.rating_id == rating_id).first()
+    if db_rating is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Rating with the given ID was not found in the DB.",
+        )
+    return db_rating
+
+
+def delete_rating_from_db(db: Session, rating: DB_Rating) -> None:
+    """Delete given rating from DB."""
+    db.delete(rating)
+    db.commit()
--- src/routes/ratings/endpoints.py
+++ src/routes/ratings/endpoints.py
@@ -2,7 +2,7 @@
 
 from typing import Annotated
 
-from fastapi import APIRouter, Body, Depends, HTTPException, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
 from sqlalchemy.orm import Session
 
 from src.db.models import DB_Rating, DB_User
@@ -11,14 +11,14 @@ from src.roles import Roles
 from src.routes.auth.utils import RoleChecker, get_current_user
 from src.tags import Tags
 
-from .crud import create_rating_in_db
+from .crud import create_rating_in_db, delete_rating_from_db, get_rating_from_db
 from .models import Rating, RatingAdd
 
 router = APIRouter(prefix="/ratings", tags=[Tags.ratings.value])
 
 
 @router.post(
-    "/ratings/add",
+    "/add",
     response_model=Rating,
     status_code=status.HTTP_201_CREATED,
     dependencies=[Depends(RoleChecker(allowed_roles=[Roles.ADMIN.value, Roles.USER.value]))],
@@ -45,3 +45,27 @@ def add_rating(
             detail="Authenticated user is the author of the recipe which prohibits them from ratings.",
         )
     return create_rating_in_db(db=db, rating_data=rating_data, author_id=current_user.user_id)
+
+
+@router.delete(
+    "/delete/{rating_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[Depends(RoleChecker(allowed_roles=[Roles.ADMIN.value, Roles.USER.value]))],
+)
+def rating_delete(
+    rating: Annotated[DB_Rating, Depends(get_rating_from_db)],
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[DB_User, Depends(get_current_user)],
+) -> None:
+    """Delete rating with the given ID.
+
+    Raises:
+        HTTPException: Raised when the authenticated user is not the the author of the
+                        rating.
+    """
+    if current_user.user_id != rating.author_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="The authenticated user is not the author of the rating.",
+        )
+    delete_rating_from_db(db=db, rating=rating)
